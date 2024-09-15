d3ced324318e0d78533c15d9ad243fc5462f92cf
NakulanT <nakult721@gmail.com>
2024-09-15 19:31:41 +0530

    Small adjustment in the DB creation

--- client/src/pages/Dashboard1.jsx
+++ client/src/pages/Dashboard1.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react';
 import TrendControls from '../components/TrendControls';
 import CowBehaviorDetails from '../components/CowBehaviorDetails/CowBehaviorDetails';
 import Comunicating from '../components/Comunicating';
+import SmallerRadiusPiechart from '../components/SmallerRadiusPiechart';
 
 const Dashboard1 = () => {
   const [trendType, setTrendType] = useState('daily');
   const [date, setDate] = useState('2022-09-15');
+    const Piedata = {
+    Standing: 12,
+    Eating: 5,
+    LyingDown: 6.6,
+    'Out of Camera': 2,
+  };
 
   return (
     <>
@@ -21,14 +28,19 @@ const Dashboard1 = () => {
           {/* Right Side: Half Width, Full Height */}
           <div className="flex flex-col w-full lg:w-1/2 gap-5 h-full">
             {/* Top Half: TrendControls */}
-            <div className="w-full h-1/2">
+            <div className="w-full h-3/4">
               <TrendControls
                 trendType={trendType}
                 setTrendType={setTrendType}
                 date={date}
                 setDate={setDate}
               />
+              
             </div>
+            <div className="w-[100%] h-1/2">
+              <SmallerRadiusPiechart data={Piedata} />              
+            </div>
+            
 
             {/* Bottom Half: Comunicating */}
             <div className="w-full h-1/2">
